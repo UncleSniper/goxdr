614ab4d7c9a1bc50eeff7d3a7caee3b5f071d582
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2023-07-15 00:53:58 +0200

    Add VariableLengthOpaqueReadState

---
+++ VariableLengthOpaqueReadState.go
@@ -0,0 +1,82 @@
+package goxdr
+
+import (
+	"fmt"
+	"errors"
+)
+
+type VariableLengthOpaqueReadState struct {
+	PrimitiveState *PrimitiveReadState
+	FixedLengthState *FixedLengthOpaqueReadState
+	inBody bool
+	firstError error
+}
+
+func(state *VariableLengthOpaqueReadState) Reset() {
+	state.PrimitiveState.Reset(4)
+	state.inBody = false
+}
+
+
+func(state *VariableLengthOpaqueReadState) Update(bytes []byte) (readCount int, isFull bool) {
+	if state.firstError != nil {
+		isFull = true
+		return
+	}
+	length := len(bytes)
+	if !state.inBody {
+		readCount, isFull = state.PrimitiveState.Update(bytes)
+		if readCount > length {
+			state.firstError = errors.New(fmt.Sprintf(
+				"Primitive read state read %d bytes, but was supposed to only read %d",
+				readCount,
+				length,
+			))
+			isFull = true
+			return
+		}
+		if !isFull {
+			return
+		}
+		state.firstError = state.PrimitiveState.EndPacket()
+		if state.firstError != nil {
+			isFull = true
+			return
+		}
+		state.FixedLengthState.Reset()
+		state.FixedLengthState.ExpectedLength = state.PrimitiveState.AsUint()
+		state.inBody = true
+	}
+	var bodyReadCount int
+	bodyReadCount, isFull = state.FixedLengthState.Update(bytes[readCount:])
+	if bodyReadCount > length - readCount {
+		state.firstError = errors.New(fmt.Sprintf(
+			"Fixed length opaque read state read %d bytes, but was supposed to only read %d",
+			bodyReadCount,
+			length - readCount,
+		))
+		isFull = true
+		return
+	}
+	readCount += bodyReadCount
+	return
+}
+
+func(state *VariableLengthOpaqueReadState) EndPacket() error {
+	if state.firstError == nil {
+		if state.inBody {
+			state.firstError = state.FixedLengthState.EndPacket()
+		} else {
+			state.firstError = state.PrimitiveState.EndPacket()
+			if state.firstError == nil {
+				state.FixedLengthState.Reset()
+				state.FixedLengthState.ExpectedLength = state.PrimitiveState.AsUint()
+				state.inBody = true
+				state.firstError = state.FixedLengthState.EndPacket()
+			}
+		}
+	}
+	return state.firstError
+}
+
+var _ ReadState = &VariableLengthOpaqueReadState{}
